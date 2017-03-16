4115b4ebc7d3262053e34a6c635bbc5e43aff532
Hleb Matsiash <nostrass@mail.ru>
2017-03-16 13:26:07 +0300

    add some changes

--- src/test/java/com/epam/util/DriverSingleton.java
+++ src/test/java/com/epam/util/DriverSingleton.java
@@ -14,7 +14,10 @@ public class DriverSingleton {
 
     private static WebDriver driver;
     private static final String WEBDRIVER_GECKO_DRIVER = "webdriver.gecko.driver";
-   private static final String GECKODRIVER_EXE_PATH = "./src/test/resources/geckodriver.exe";
+    private static final String GECKODRIVER_EXE_PATH = "./src/test/resources/geckodriver.exe";
+
+    private static final String WEBDRIVER_CHROME_DRIVER = "webdriver.chrome.driver";
+    private static final String CHROMEDRIVER_EXE_PATH = "./src/test/resources/chromedriver.exe";
 
     private DriverSingleton() {
 
@@ -23,8 +26,10 @@ public class DriverSingleton {
 
     public static WebDriver getDriver() {
         if (null == driver) {
-            System.setProperty(WEBDRIVER_GECKO_DRIVER, GECKODRIVER_EXE_PATH);
-            driver = new FirefoxDriver();
+            //System.setProperty(WEBDRIVER_GECKO_DRIVER, GECKODRIVER_EXE_PATH);
+            System.setProperty(WEBDRIVER_CHROME_DRIVER, CHROMEDRIVER_EXE_PATH);
+            //driver = new FirefoxDriver();
+            driver = new ChromeDriver();
             driver.manage().timeouts().pageLoadTimeout(35, TimeUnit.SECONDS);
             driver.manage().timeouts().implicitlyWait(35, TimeUnit.SECONDS);
             driver.manage().window().maximize();
